92a8b7f4fd56867af9ea946b14c2fe04b084c338
Renzo Banegas <renzobanegas720@hotmail.com>
2021-08-02 20:02:23 -0300

    commit-FizzBuzz-main-project

---
+++ FizzBuzzKata/FizzBuzz.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace FizzBuzzKata
+{
+    public class FizzBuzzKata
+    {
+        static void Main(string[] args)
+        {
+
+            object[] FizzBuzzList = GetList();
+
+            for (int i = 0; i < FizzBuzzList.Length; i++)
+            {
+                Console.WriteLine(FizzBuzzList[i]);
+            }
+            
+        }
+
+        public static object[] GetList()
+        {
+            object[] List = new object[20];
+
+            for (int i = 0; i < List.Length; i++)
+            {
+                int number = i + 1;
+
+                if(number % 3 == 0 && number % 5 == 0)
+                {
+                    List[i] = "FizzBuzz";
+                }
+                else if(number % 3 == 0)
+                {
+                    List[i] = "Fizz";
+                }
+                else if(number % 5 == 0)
+                {
+                    List[i] = "Buzz";
+                }
+                else
+                {
+                    List[i] = number;
+                }                
+            }            
+
+            return List;
+        }
+    }
+}
